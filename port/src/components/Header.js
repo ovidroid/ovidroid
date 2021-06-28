a440940340207ac7c09dca50b9af019a5ebfcba6
Sayyed Owais <sayyedo963@gmail.com>
2021-06-27 22:15:14 -0700

    First Commit

---
+++ port/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header(){
+    return(
+<div className ="slideshow-container">
+
+    
+       <h1>Test</h1>
+    
+
+      
+      </div>
+
+
+
+    )
+}
+export default  Header
